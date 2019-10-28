a3d0a38c52dd5a2c59fb950f172b0df1a1d252f8
Rob Weber <rob.weber87@gmail.com>
2019-10-28 00:34:40 -0400

    working project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className='homepage-view'>
+        <div>
+          <header className='std-header'>SAVE OUR DATE</header>
+        </div>
+        <div className='top-border-line'></div>
+        <div>
+          <p className='std-subheader'>CUZ WE'RE DOING IT. AGAIN.</p>
+        </div>
+        <div className='bottom-border-line'></div>
+        <div className='date'>
+          <div className='month-year'>SEPT</div>
+          <div className='left-border-line'></div>
+          <div className='day'>05</div>
+          <div className='right-border-line'></div>
+          <div className='month-year'>2020</div>
+        </div>
+        <div className='location'>
+          MUNICH, GERMANY
+        </div>
+        <div className='invite-text'>FORMAL INVITATION TO FOLLOW</div>
+        <div className='stay-tuned-text'>STAY TUNED - MORE INFO COMING SOON!</div>
+      </div>
+    );
+  }
 }
 
 export default App;
