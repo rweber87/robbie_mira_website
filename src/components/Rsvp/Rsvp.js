53da59d25dca1b6d2d21c3eebb5e785c85a7f949
Rob Weber <rob.weber87@gmail.com>
2020-01-22 23:27:23 -0500

    added env var and updated rsvp section

--- src/components/Rsvp/Rsvp.js
+++ src/components/Rsvp/Rsvp.js
@@ -6,7 +6,8 @@ class Rsvp extends Component {
     super(props);
 
     this.state = {
-      email: '',
+      allergyComments: '',
+      confirmCode: '',
       firstName: '',
       isGoing: false,
       isNotGoing: false,
@@ -28,80 +29,111 @@ class Rsvp extends Component {
     });
   };
 
+  isButtonActive = () => {
+    const {
+      confirmCode,
+      firstName,
+      isGoing,
+      isNotGoing,
+      lastName
+    } = this.state;
+    let envConfirmCode = process.env.REACT_APP_CONFIRM_CODE;
+    if (
+      firstName &&
+      lastName &&
+      (isGoing || isNotGoing) &&
+      confirmCode === envConfirmCode
+    ) {
+      return true;
+    }
+    return false;
+  };
+
   handleChange = (e, { name, value }) => this.setState({ [name]: value });
 
   onSubmit = () => {};
 
   render() {
     let { firstName, isGoing, isNotGoing } = this.state;
-    console.log('firstName', firstName);
+    let isButtonActive = this.isButtonActive();
+
     return (
-      <div className='outer-ui-form'>
-        <Form>
-          <Form.Group widths='equal'>
-            <Form.Input
-              fluid
-              label='First name'
-              name='firstName'
-              onChange={this.handleChange}
-            />
-            <Form.Input
-              fluid
-              label='Last name'
-              name='lastName'
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Input
-              fluid
-              label='Email'
-              width={8}
-              name='email'
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Input
-              fluid
-              label='Code (can be found on your invitation)'
-              width={8}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Field
-              className='attend'
-              label='Will you attend?'
-            ></Form.Field>
-          </Form.Group>
-          <Form.Group label='Will you attend?' inline>
-            <Form.Checkbox
-              label='Count Me In!'
-              checked={isGoing}
-              onChange={this.isGoingFunction}
-            />
-            <Form.Checkbox
-              className='pooper'
-              label='Count Me Out :('
-              checked={isNotGoing}
-              onChange={this.isNotGoingFunction}
-            />
-          </Form.Group>
-          {isGoing && (
-            <div>
-              <Form.Group widths='equal'>
-                <Form.Input fluid label='Full Address' />
-              </Form.Group>
-              <Form.Group widths='equal'>
-                <Form.TextArea
-                  fluid
-                  label='List any food allergies (if applicable)'
-                />
-              </Form.Group>
-            </div>
-          )}
-          <Form.Button className='submit-button'>SUBMIT</Form.Button>
-        </Form>
+      <div>
+        <div className='welcome-text' style={{ paddingBottom: 0 }}>
+          <div className='welcome-text-paragraph' style={{ paddingBottom: 0 }}>
+            We kindly ask every person planning to attend the wedding to RSVP
+            individually (except the kiddies of course)
+          </div>
+        </div>
+        <div className='outer-ui-form'>
+          <Form>
+            <Form.Group widths='equal'>
+              <Form.Input
+                fluid
+                required
+                label='First name'
+                name='firstName'
+                onChange={this.handleChange}
+              />
+              <Form.Input
+                fluid
+                required
+                label='Last name'
+                name='lastName'
+                onChange={this.handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Input
+                fluid
+                required
+                label='RSVP Code (can be found on your invitation)'
+                name='confirmCode'
+                onChange={this.handleChange}
+                width={8}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Field
+                required
+                className='attend'
+                label='Will you attend?'
+              ></Form.Field>
+            </Form.Group>
+            <Form.Group label='Will you attend?' inline>
+              <Form.Checkbox
+                label='Count Me In!'
+                checked={isGoing}
+                onChange={this.isGoingFunction}
+              />
+              <Form.Checkbox
+                className='pooper'
+                label='Count Me Out :('
+                checked={isNotGoing}
+                onChange={this.isNotGoingFunction}
+              />
+            </Form.Group>
+            {isGoing && (
+              <div>
+                <Form.Group widths='equal'>
+                  <Form.TextArea
+                    fluid
+                    label='List any food allergies (if applicable)'
+                    name='allergyComments'
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+              </div>
+            )}
+            <Form.Button
+              active={isButtonActive}
+              className='submit-button'
+              onClick={this.isButtonActive}
+            >
+              SUBMIT
+            </Form.Button>
+          </Form>
+        </div>
       </div>
     );
   }
