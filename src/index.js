a4f5c707f8160e7d376fa498d337ef91e55ec6e8
Rob Weber <rob.weber87@gmail.com>
2020-02-08 15:38:01 -0500

    last update for URL

--- src/index.js
+++ src/index.js
@@ -7,13 +7,10 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 
-const aws = require('aws-sdk');
-const API_URL = new aws.S3({
-  api: process.env.api
+const client = new ApolloClient({
+  uri: `https://peaceful-bastion-92264.herokuapp.com/graphql`
 });
 
-const client = new ApolloClient({ uri: `${process.env.API_URL}/graphql` });
-console.log('API_URL', process.env);
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Router>
