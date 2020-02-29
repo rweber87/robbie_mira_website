89b539fa29a36c1fe92b579b64c1b3229a160dcf
Rob Weber <rob.weber87@gmail.com>
2020-02-28 19:56:16 -0500

    fix hilton booking link

--- src/components/Lodging/Lodging.js
+++ src/components/Lodging/Lodging.js
@@ -50,7 +50,7 @@ export default function Lodging() {
         Click{' '}
         <a
           className='events-href'
-          href='https://secure3.hilton.com/en_US/hi/reservation/book.htm?execution=e1s1'
+          href={`https://nam02.safelinks.protection.outlook.com/?url=https%3A%2F%2Fsecure3.hilton.com%2Fen_US%2Fhi%2Freservation%2Fbook.htm%3Fctyhocn%3DMUCHITW%26arrivalDay%3D29%26arrivalMonth%3D08%26arrivalYear%3D2020%26departureDay%3D07%26departureMonth%3D09%26departureYear%3D2020%26spec_plan%3DGMIRA%26datesFlex%3Dfalse%26cid%3DOM%2CWW%2CHILTONLINK%2CEN%2CDirectLink%26fromId%3DHILTONLINKDIRECT&data=02%7C01%7Ccelina.weigert%40hilton.com%7C0ca7eeb2c9c14fd4f77308d7b8fea66f%7C660292d2cfd54a3db7a7e8f7ee458a0a%7C0%7C0%7C637181276162685887&sdata=GX5IH0VyDE6Ukca5%2BHq0s%2BuXNTjre9YA0pBtyb5fdUY%3D&reserved=0`}
           rel='noopener noreferrer'
           style={{ color: 'white' }}
           target='_blank'
